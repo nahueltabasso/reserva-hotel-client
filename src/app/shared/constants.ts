5c84aedc59d89bdcc87a991ced1867a8594ce11b
nahueltabasso <nahueltabasso@gmail.com>
2021-02-21 16:08:07 -0300

    se agregaron nuevas constantes

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -4,6 +4,9 @@ import { ValidatorFn, FormGroup, ValidationErrors } from '@angular/forms';
 export const PATTERN_ONLYLETTERS = '^[a-zA-Z\s]*$';
 export const PATTERN_ONLYNUMBER = '^[0-9]*$';
 
+export const MIN_DNI = 10000000;
+export const MAX_DNI = 100000000;
+
 export const validEqualsPasswords: ValidatorFn = (
     control: FormGroup
 ): ValidationErrors | null => {
