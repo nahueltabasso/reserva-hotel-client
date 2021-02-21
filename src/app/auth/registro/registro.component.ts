4aabd7d74bfb5e79a2fe590a6c10b114a584f744
nahueltabasso <nahueltabasso@gmail.com>
2021-02-21 16:07:52 -0300

    componente registro

--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -3,7 +3,8 @@ import { AuthService } from '../../services/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Persona } from '../../models/model';
-import { PATTERN_ONLYLETTERS, PATTERN_ONLYNUMBER, validEqualsPasswords } from '../../shared/constants';
+import { PATTERN_ONLYLETTERS, PATTERN_ONLYNUMBER, validEqualsPasswords, MIN_DNI, MAX_DNI } from '../../shared/constants';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -31,7 +32,7 @@ export class RegistroComponent implements OnInit {
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.required],
       passwordConfirm: ['', Validators.required],
-      nroDocumento: ['', Validators.compose([Validators.required, Validators.pattern(PATTERN_ONLYNUMBER)])],
+      nroDocumento: ['', Validators.compose([Validators.required, Validators.pattern(PATTERN_ONLYNUMBER), Validators.min(MIN_DNI), Validators.max(MAX_DNI)])],
       tipoDocumento: ['', Validators.required],
       cuit: ['', Validators.compose([Validators.required, Validators.pattern(PATTERN_ONLYNUMBER)])],
       telefono: ['', Validators.compose([Validators.required, Validators.pattern(PATTERN_ONLYNUMBER)])],
@@ -41,8 +42,38 @@ export class RegistroComponent implements OnInit {
     });
   }
 
+  public seleccionarTipoDocumento(event) {
+    this.personaDTO.tipoDocumento = event;
+  }
+
+  public seleccionarGenero(event) {
+    this.personaDTO.genero = event
+  }
+
   public registrarNuevoUsuario() {
     if (this.formulario.invalid) return;
+    const { nombre, apellido, email, password, nroDocumento, cuit, telefono } = this.formulario.value;
+    this.personaDTO.nombre = nombre;
+    this.personaDTO.apellido = apellido;
+    this.personaDTO.email = email;
+    this.personaDTO.password = password;
+    this.personaDTO.nroDocumento = nroDocumento;
+    this.personaDTO.cuit = cuit;
+    this.personaDTO.telefono = telefono;
+    console.log(this.personaDTO);
     
+    return;
+    this.authService.registracion(this.personaDTO).subscribe(data => {
+      Swal.fire('Registrado!', 'Usuario registrado correctamente', 'success');
+      this.router.navigate(['/login']);
+    }, (err) => {
+      Swal.fire('Error!', 'Ocurrio un error!', 'error');
+    });
+  }
+
+  public checkPassword(): boolean {
+    return this.formulario.hasError('notEquals') &&
+           this.formulario.get('password').dirty &&
+           this.formulario.get('passwordConfirm').dirty;
   }
 }
